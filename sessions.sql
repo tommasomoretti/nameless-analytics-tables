8242720d6dbd6826fdae64052533f1557f809e55
Tommaso Moretti <tommasomoretti88@gmail.com>
2025-06-25 22:53:59 +0200

    Update sessions.sql

--- sessions.sql
+++ sessions.sql
@@ -1,5 +1,5 @@
 CREATE OR REPLACE TABLE FUNCTION `tom-moretti.nameless_analytics.sessions`(start_date DATE, end_date DATE) AS (
-with session_data_raw as ( 
+  with session_data_raw as ( 
     select
       -- USER DATA
       user_data.user_date,
