84ca7001a99f3ca20d6eccc495f0f7af7510b066
Tommaso Moretti <tommasomoretti88@gmail.com>
2025-06-28 20:34:37 +0200

    Update consents.sql

--- consents.sql
+++ consents.sql
@@ -1,15 +1,47 @@
 CREATE OR REPLACE TABLE FUNCTION `tom-moretti.nameless_analytics.consents`(start_date DATE, end_date DATE) AS (
-  with consent_data as (
+  with event_data as (
     SELECT
+      -- USER DATA
+      user_date,
+      client_id,
+      user_id,
+      user_channel_grouping,
+      user_source,
+      user_tld_source,
+      user_campaign,
+      user_device_type,
+      user_country,
+      user_language,
+      user_type,
+      new_user,
+      returning_user,
+  
+      -- SESSION DATA
       session_date,
+      session_number,
       session_id,
-      session_channel_grouping, 
-      original_session_source,
+      session_start_timestamp,
+      session_duration_sec,
+      first_session,
+      engaged_session,
+      session_channel_grouping,
       session_source, 
-      session_campaign, 
-      session_device_type, 
-      session_country, 
+      session_tld_source,
+      session_campaign,
+      session_device_type,
+      session_country,
+      session_browser_name,
       session_language,
+      cross_domain_session,
+      session_landing_page_category,
+      session_landing_page_location,
+      session_landing_page_title,
+      session_exit_page_category,
+      session_exit_page_location,
+      session_exit_page_title,
+      session_hostname,
+
+      -- CONSENT DATA
       case 
         when consent_expressed = 'Yes' then 'Consent expressed'
         else'Consent not expressed'
@@ -24,41 +56,73 @@ CREATE OR REPLACE TABLE FUNCTION `tom-moretti.nameless_analytics.consents`(start
   )
 
   select 
-      session_date,
-      session_id,
-      session_channel_grouping, 
-      original_session_source,
-      session_source, 
-      session_campaign, 
-      session_device_type, 
-      session_country, 
-      session_language,
-      consent_state,
-      case 
-        when consent_state = 'Consent expressed' then session_id
-        else null
-      end as session_id_consent_expressed,
-      case 
-        when consent_state = 'Consent not expressed' then session_id
-        else null
-      end as session_id_consent_not_expressed,
-      consent_name,
-      case 
-        when consent_state = 'Consent expressed' and consent_value_int_accepted = 1 then 'Granted'
-        when consent_state = 'Consent expressed' and consent_value_int_accepted = 0 then 'Denied'
-        -- when consent_state = 'Consent not expressed' then ''
-      end as consent_value_string,
-      consent_value_int_accepted,
-      case 
-        when consent_state = 'Consent expressed' and consent_name = "session_ad_user_data" and consent_value_int_accepted = 0 then 1
-        when consent_state = 'Consent expressed' and consent_name = "session_ad_personalization" and consent_value_int_accepted = 0 then 1
-        when consent_state = 'Consent expressed' and consent_name = "session_ad_storage" and consent_value_int_accepted = 0 then 1
-        when consent_state = 'Consent expressed' and consent_name = "session_analytics_storage" and consent_value_int_accepted = 0 then 1
-        when consent_state = 'Consent expressed' and consent_name = "session_functionality_storage" and consent_value_int_accepted = 0 then 1
-        when consent_state = 'Consent expressed' and consent_name = "session_personalization_storage" and consent_value_int_accepted = 0 then 1
-        when consent_state = 'Consent expressed' and consent_name = "session_security_storage" and consent_value_int_accepted = 0 then 1
-        else 0
-      end as consent_value_int_denied,
-  from consent_data
+    -- USER DATA
+    user_date,
+    client_id,
+    user_id,
+    user_channel_grouping,
+    user_source,
+    user_tld_source,
+    user_campaign,
+    user_device_type,
+    user_country,
+    user_language,
+    user_type,
+    new_user,
+    returning_user,
+  
+    -- SESSION DATA
+    session_date,
+    session_number,
+    session_id,
+    session_start_timestamp,
+    session_duration_sec,
+    first_session,
+    engaged_session,
+    session_channel_grouping,
+    session_source, 
+    session_tld_source,
+    session_campaign,
+    session_device_type,
+    session_country,
+    session_browser_name,
+    session_language,
+    cross_domain_session,
+    session_landing_page_category,
+    session_landing_page_location,
+    session_landing_page_title,
+    session_exit_page_category,
+    session_exit_page_location,
+    session_exit_page_title,
+    session_hostname,
+
+    -- CONSENT DATA
+    consent_state,    
+    case 
+      when consent_state = 'Consent expressed' then session_id
+      else null
+    end as session_id_consent_expressed,
+    case 
+      when consent_state = 'Consent not expressed' then session_id
+      else null
+    end as session_id_consent_not_expressed,
+    consent_name,
+    case 
+      when consent_state = 'Consent expressed' and consent_value_int_accepted = 1 then 'Granted'
+      when consent_state = 'Consent expressed' and consent_value_int_accepted = 0 then 'Denied'
+      -- when consent_state = 'Consent not expressed' then ''
+    end as consent_value_string,
+    consent_value_int_accepted,
+    case 
+      when consent_state = 'Consent expressed' and consent_name = "session_ad_user_data" and consent_value_int_accepted = 0 then 1
+      when consent_state = 'Consent expressed' and consent_name = "session_ad_personalization" and consent_value_int_accepted = 0 then 1
+      when consent_state = 'Consent expressed' and consent_name = "session_ad_storage" and consent_value_int_accepted = 0 then 1
+      when consent_state = 'Consent expressed' and consent_name = "session_analytics_storage" and consent_value_int_accepted = 0 then 1
+      when consent_state = 'Consent expressed' and consent_name = "session_functionality_storage" and consent_value_int_accepted = 0 then 1
+      when consent_state = 'Consent expressed' and consent_name = "session_personalization_storage" and consent_value_int_accepted = 0 then 1
+      when consent_state = 'Consent expressed' and consent_name = "session_security_storage" and consent_value_int_accepted = 0 then 1
+      else 0
+    end as consent_value_int_denied,
+  from event_data
   group by all
 );
